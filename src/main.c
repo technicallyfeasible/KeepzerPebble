00a70d5fd4702413e0b8a9917e859702a10a85e1
technicallyfeasible <jens.elstner@technicallyfeasible.com>
2013-11-30 06:11:06 -0800

    Fixed compile error

--- src/main.c
+++ src/main.c
@@ -5,14 +5,23 @@
 static Window *window;
 static TextLayer *text_layer;
 static PropertyAnimation *prop_animation;
-static TextLayer *text_layer;
+
+static GRect bounds;
+static Layer *navi_layer;
+static TextLayer *confirm_text_layer;
+static TextLayer *sync_text;
+static TextLayer *item_text_1;
+static TextLayer *item_text_2;
+
+static GBitmap *arrow_up_image;
+static GBitmap *arrow_down_image;
 
 static void animation_started(Animation *animation, void *data) {
-  text_layer_set_text(text_layer, "Started.");
+  //text_layer_set_text(text_layer, "Started.");
 }
 
 static void animation_stopped(Animation *animation, bool finished, void *data) {
-  text_layer_set_text(text_layer, finished ? "Hi, I'm a TextLayer!" : "Just Stopped.");
+  //text_layer_set_text(text_layer, finished ? "Hi, I'm a TextLayer!" : "Just Stopped.");
 }
 
 static void destroy_property_animation(PropertyAnimation **prop_animation) {
@@ -29,7 +38,7 @@ static void destroy_property_animation(PropertyAnimation **prop_animation) {
 }
 
 static void click_handler(ClickRecognizerRef recognizer, Window *window) {
-  Layer *layer = text_layer_get_layer(text_layer);
+  /*Layer *layer = text_layer_get_layer(text_layer);
 
   GRect to_rect;
   if (toggle) {
@@ -63,7 +72,23 @@ static void click_handler(ClickRecognizerRef recognizer, Window *window) {
     .started = (AnimationStartedHandler) animation_started,
     .stopped = (AnimationStoppedHandler) animation_stopped,
   }, NULL);
-  animation_schedule((Animation*) prop_animation);
+  animation_schedule((Animation*) prop_animation);*/
+}
+
+static void load_resources() {
+  arrow_up_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ARROW_UP);
+  arrow_down_image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_ARROW_DOWN);
+}
+
+static void create_navi(Window *window) {
+  navi_layer = layer_create(GRect(0,0, 48, bounds.size.h));
+  
+  confirm_text_layer = text_layer_create(GRect(0, 0, 48, bounds.size.h));
+  text_layer_set_text(confirm_text_layer, "log");
+  layer_add_child(navi_layer, text_layer_get_layer(confirm_text_layer));
+
+
+  layer_add_child(window_get_root_layer(window), navi_layer);
 }
 
 static void config_provider(Window *window) {
@@ -77,19 +102,18 @@ static void init(void) {
   window_set_click_config_provider(window, (ClickConfigProvider) config_provider);
   window_stack_push(window, false);
 
-  text_layer = text_layer_create(GRect(0, 0, 60, 60));
-	
-  text_layer_set_text(text_layer, "Started!");
+  Layer *window_layer = window_get_root_layer(window);
+  // Get the bounds of the window for sizing the text layer
+  bounds = layer_get_bounds(window_layer);
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Toggle is %u, %p", toggle, &toggle);
-
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+  load_resources();
+  create_navi(window);
 
   GRect to_rect = GRect(84, 92, 60, 60);
 
-  prop_animation = property_animation_create_layer_frame(text_layer_get_layer(text_layer), NULL, &to_rect);
+  //prop_animation = property_animation_create_layer_frame(text_layer_get_layer(text_layer), NULL, &to_rect);
 
-  animation_schedule((Animation*) prop_animation);
+  //animation_schedule((Animation*) prop_animation);
 }
 
 static void deinit(void) {
